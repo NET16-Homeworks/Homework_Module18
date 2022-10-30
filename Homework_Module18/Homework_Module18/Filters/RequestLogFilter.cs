8c95d1e51383e8914928534ae47c924bda1a5685
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-10-30 21:06:59 +0300

    Create RequestLogFilter filter

---
+++ Homework_Module18/Homework_Module18/Filters/RequestLogFilter.cs
@@ -0,0 +1,22 @@
+﻿using Homework_Module18.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Homework_Module18.Filters
+{
+    public sealed class RequestLogFilter : Attribute, IActionFilter
+    {
+        private RequestInfo _requestInfo = new RequestInfo();
+        private UrlLogger _urlLogger = new UrlLogger();
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _requestInfo.IsSuccessful = context.Exception == null;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.Controller.GetType().Name;
+            _requestInfo.Url = context.HttpContext.Request.Path.ToString();
+            _urlLogger.PutRequest(controller, _requestInfo);
+        }
+    }
+}
